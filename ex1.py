0dd9abd637e3284ef40c36aa88491eea132279b2
adityaaa901 <adityakumar8809964@gmail.com>
2026-06-09 21:40:58 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,20 @@
+age = int(input("Apni age enter kijiye: "))
+
+if age <= 18:
+    print("You are not eligible to vote")
+else:
+    print("You are eligible to vote")
+while True:
+    age = int(input("Apni age enter kijiye: "))
+    
+    if age <= 18:
+        print("You are not eligible to vote")
+    else:
+        print("You are eligible to vote")
+    
+    # पूछो कि दोबारा check करना है या नहीं
+    again = input("Kya dobara check karna hai? (yes/no): ")
+    if again.lower() != 'yes':
+        print("Thank you!")
+        break
+    
